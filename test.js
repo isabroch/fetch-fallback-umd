bcb8eda8936326b5133894b9b624ca6316fdff38
Isabella Broch <5460278@RTS-365.dk>
2019-12-18 12:11:07 +0100

    get method node https, fetch, or xmlhttprequest

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 let myFetch = require("./umd");
 
-myFetch.get();
+myFetch.init({
+  address: "https://reqres.in/api/",
+  key: "1234"
+});
+
+// METHOD GET
+myFetch.get("users/")
+  .then(result => console.log(result));
--- umd.js
+++ umd.js
@@ -17,24 +17,44 @@
   // our own part of the module
 
   function init(options) {
-    this.APIAdress = options.address;
+    this.APIAddress = options.address;
     this.APIKey = options.key;
     return this;
   }
 
-  function isSupported(feature) {
-    return feature in window;
-  }
-
-
   async function get(resource) {
-    let linky = this.APIAdress + resource;
-
     try {
 
-      // FETCH METHOD
-      if ((isSupported('fetch'))) {
-        let response = await fetch(linky, {
+      // NODE METHOD GET
+      if (typeof exports === "object") {
+        const httpsNode = require('https');
+
+        const options = {
+          method: 'GET',
+          headers: {
+            "Authorization": this.APIKey
+          }
+        }
+
+        const req = httpsNode.request(this.APIAddress + resource, options, async(response) => {
+          response.setEncoding('utf8');
+
+          let rawData = '';
+
+          response.on('data', (chunk) => { rawData += chunk; });
+          response.on('end', () => { console.log(JSON.parse(rawData)); });
+        });
+
+        req.on('error', (err) => {
+          console.error(err);
+        })
+
+        req.end();
+      }
+
+      // FETCH METHOD GET
+      else if (typeof fetch !== 'undefined') {
+        let response = await fetch(this.APIAddress + resource, {
           headers: {
             "Authorization": this.APIKey
           }
@@ -43,27 +63,27 @@
         return await response.json();
       }
 
-      // XML METHOD
-      else if ((isSupported('XMLHttpRequest'))) {
-        // Returning promise to match earlier formatting
-        return new Promise(function (resolve, reject) {
+      // XML METHOD GET
+      else if (typeof XMLHttpRequest !== 'undefined') {
+        // Creates HTTP request template
+        const xhttp = new XMLHttpRequest();
 
-          // Creates HTTP request template
-          const xhttp = new XMLHttpRequest();
+        // What should the HTTP request do?
+        xhttp.open("GET", this.APIAddress + resource, true);
 
+        // Sending authorization to API
+        xhttp.setRequestHeader("Authorization", this.APIkey)
+
+        // Sends formatted HTTP request
+        xhttp.send();
+
+        return await new Promise(function (resolve, reject) {
           // When response is ready, what to do with response?
           xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-              Promise.resolve(xhttp.responseText);
+              resolve(JSON.parse(xhttp.responseText));
             }
           };
-
-          // What should the HTTP request do?
-          xhttp.open("GET", linky, true);
-
-          // Sends formatted HTTP request
-          xhttp.send();
-
         })
       }
 
@@ -72,8 +92,52 @@
     }
   }
 
+  async function post(resource, data) {
+    try {
+      // FETCH METHOD POST
+      if (typeof fetch !== 'undefined') {
+        let response = await fetch(this.APIAddress + resource, {
+          headers: {
+            "Authorization": this.APIKey,
+            "Content-Type": "application/json"
+          },
+          method: "POST",
+          body: JSON.stringify(data)
+        });
+
+        return await response.json();
+      }
+
+      // XML METHOD POST
+      else if (typeof XMLHttpRequest !== 'undefined') {
+        // Creates HTTP request template
+        const xhttp = new XMLHttpRequest();
+
+        // What should the HTTP request do?
+        xhttp.open("POST", this.APIAddress + resource, true);
+
+        // Sends formatted HTTP request
+        xhttp.send();
+
+        return await new Promise(function (resolve, reject) {
+          // When response is ready, what to do with response?
+          xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+              resolve(JSON.parse(xhttp.responseText));
+            }
+          };
+        })
+      }
+
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
   return {
     init,
-    get
+    get,
+    post
   }
 }));
